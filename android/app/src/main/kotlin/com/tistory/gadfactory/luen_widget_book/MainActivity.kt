838709d31a08e2a5a50c3b49856ecf60295baf3c
Luen <thegad2003@gmail.com>
2024-01-10 11:15:42 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/tistory/gadfactory/luen_widget_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tistory.gadfactory.luen_widget_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:luen_widget_book/main_page.dart';
+import 'package:get/get.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() {
+  setPathUrlStrategy(); //url # 제거
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: '루엔 위젯 북',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Pretendard',
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+      ],
+      initialRoute: MainPage.route,
+      getPages: [
+        GetPage(name: MainPage.route, page: () => const MainPage()),
+      ],
+    );
+  }
+}
